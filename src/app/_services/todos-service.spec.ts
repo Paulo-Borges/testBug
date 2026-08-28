a762e8655a2a07251daad36485e6e600e2f7e4e0
Borges <borgespaulo72@yahoo.com.br>
2026-08-28 15:44:20 -0300

    feat: Testando com MOCK

--- src/app/_services/todos-service.spec.ts
+++ src/app/_services/todos-service.spec.ts
@@ -1,16 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import { TODOS_STRING, TODO_STRING } from '../../../server/db-data';
 import { TodosService } from './todos-service';
 import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { environment } from '../../environments/environment';
 
 describe('TodosService', () => {
   let todosService: TodosService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TodosService, provideHttpClient()],
+      providers: [TodosService, provideHttpClient(), provideHttpClientTesting()],
     });
     todosService = TestBed.inject(TodosService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
@@ -25,6 +29,10 @@ describe('TodosService', () => {
       const todo = todos.find((todo) => todo.id === 15);
       expect(todo?.title).toEqual('ab voluptatum amet voluptas');
     });
+
+    const req = httpTestingController.expectOne(environment.apiUrl + 'todos');
+    expect(req.request.method).toEqual('GET');
+    req.flush(JSON.parse(TODOS_STRING));
   });
 
   //___________X____teste no EndPoint que retorna todos os todos
@@ -33,5 +41,9 @@ describe('TodosService', () => {
       expect(todo).toBeTruthy();
       expect(todo.id).toEqual(12);
     });
+
+    const req = httpTestingController.expectOne(environment.apiUrl + 'todos/12');
+    expect(req.request.method).toEqual('GET');
+    req.flush(JSON.parse(TODO_STRING));
   });
 });
