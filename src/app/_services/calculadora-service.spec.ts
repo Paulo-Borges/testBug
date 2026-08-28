d46d8c0756c0e0735f29ee01ce5a4db267aaf04b
Borges <borgespaulo72@yahoo.com.br>
2026-08-28 09:36:06 -0300

    feat: testando se existe

--- src/app/_services/calculadora-service.spec.ts
+++ src/app/_services/calculadora-service.spec.ts
@@ -2,14 +2,19 @@ import { TestBed } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora-service';
 
+//_______________________________X__________________Vai decrever o teste
 describe('CalculadoraService', () => {
   let service: CalculadoraService;
 
+  //____E executado(12, 13) antes de cada teste(18),
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [CalculadoraService],
+    });
     service = TestBed.inject(CalculadoraService);
   });
 
+  //_______________________________X__________________É um teste(18) que verifica se foi criado corretamente
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
--- src/app/_services/calculadora-service.ts
+++ src/app/_services/calculadora-service.ts
@@ -3,4 +3,19 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class CalculadoraService {}
+export class CalculadoraService {
+  calcular(num1: number, num2: number, operacao: string) {
+    switch (operacao) {
+      case 'soma':
+        return num1 + num2;
+      case 'subtração':
+        return num1 - num2;
+      case 'divisão':
+        return num1 / num2;
+      case 'multiplicação':
+        return num1 * num2;
+      default:
+        return null;
+    }
+  }
+}
